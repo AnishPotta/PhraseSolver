c79b8b80b1801b9033ac547327a7051856927733
AnishPotta <142432544+AnishPotta@users.noreply.github.com>
2023-11-13 11:58:51 -0800

    Update PhraseSolver.java

--- PhraseSolver.java
+++ PhraseSolver.java
@@ -7,11 +7,29 @@ import java.util.Scanner;
   
 public class PhraseSolver
 {
-  /* your code here - attributes */
-
+  /* your code here - attributes */ 
+ private Player player_1;
+ private Player player_2;
+ private BoardGame boardGame;
+ private boolean solved ;
+  
   /* your code here - constructor(s) */ 
-
+public PhraseSolver 
+  this.player_1 = newPlayer()
+  this.player_2 = newPlayer()
+  this.boardgame = new boardgame();
+  solved = false;
+  
   /* your code here - accessor(s) */
+public Players getPlayers(int PlayerNum){
+  if (playerNum == 1) {
+  return player_1;
+} else if (playerNum == 2) {
+  return player_2;
+}
+return player_2;
+}
+
   
   /* your code here - mutator(s)  */
 
@@ -27,6 +45,8 @@ public class PhraseSolver
     {
       
       /* your code here - game logic */
+
+      
       
       
       /* your code here - determine how game ends */
